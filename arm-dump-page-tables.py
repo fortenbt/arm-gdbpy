6cd43722785fa8376ec67726556f404927500e40
Ben Fortener <fortenbt@gmail.com>
2014-02-09 22:10:56 -0500

    Coarse page tables with small pages should work.

--- arm-dump-page-tables.py
+++ arm-dump-page-tables.py
@@ -13,22 +13,37 @@ class PageTables(gdb.Command):
     def __init__(self):
         super(PageTables, self).__init__('dump-page-tables', gdb.COMMAND_DATA)
 
-    def _handle_l1_coarse(self, anInt, inf, l1, ttb_index):
+
+    def _handle_large_page(self, out, ttb_index, l2, pgtbl_index):
+        pass
+
+    def _handle_small_page(self, out, ttb_index, l2, pgtbl_index):
+        page = ttb_index & 0xfffff000
+        vpage = (ttb_index << 20) | (pgtbl_index << 12)
+        s = '0x{0:08X},0x{1:08X}'.format(vpage, page)
+        b = bytearray(s)
+        for x in b: out.append(x)
+
+    def _handle_tiny_page(self, out, ttb_index, l2, pgtbl_index):
+        pass
+
+    def _handle_l1_coarse(self, out, anInt, inf, l1, ttb_index):
         pgtbl = inf.read_memory(l1 & 0xfffffc00, 256*4)
         for i in xrange(256):
             l2 = anInt.unpack_from(pgtbl, i*4)
             if not l2: continue
 
             if l2 & 0x3 == L2_DESCRIPTOR_LARGE:
-                self._handle_large_page(ttb_index, l2, i*4)
+                self._handle_large_page(out, ttb_index, l2, i*4)
             elif l2 & 0x3 == L2_DESCRIPTOR_SMALL:
-                self._handle_small_page(ttb_index, l2, i*4)
+                self._handle_small_page(out, ttb_index, l2, i*4)
             elif l2 & 0x3 == L2_DESCRIPTOR_TINY:
-                self._handle_tiny_page(ttb_index, l2, i*4)
+                self._handle_tiny_page(out, ttb_index, l2, i*4)
             else:
                 raise gdb.GdbError('unknown L2 descriptor value: 0x{0:X}'.format(l2 & 0x3)
 
     def _dump_page_tables(self, fname, ttb):
+        out = []
         anInt = struct.Struct('<I')
         inf = gdb.inferiors()[0]
 
@@ -38,11 +53,11 @@ class PageTables(gdb.Command):
             if not l1: continue
 
             if l1 & 0x3 == L1_DESCRIPTOR_COARSE:
-                self._handle_l1_coarse(anInt, inf, l1, i*4)
+                self._handle_l1_coarse(out, anInt, inf, l1, i*4)
             elif l1 & 0x3 == L1_DESCRIPTOR_SECTION:
-                self._handle_l1_section(anInt, inf, l1, i*4)
+                self._handle_l1_section(out, anInt, inf, l1, i*4)
             elif l1 & 0x3 == L1_DESCRIPTOR_FINE:
-                self._handle_l1_fine(anInt, inf, l1, i*4)
+                self._handle_l1_fine(out, anInt, inf, l1, i*4)
             else:
                 raise gdb.GdbError('unknown L1 descriptor value: 0x{0:X}'.format(l1 & 0x3)
 
